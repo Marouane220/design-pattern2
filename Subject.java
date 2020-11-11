58606804347c8994b63ed6de24eac9b48b2f5734
Marouane Maâou <65347450+Marouane220@users.noreply.github.com>
2020-11-11 09:07:48 +0100

    Update Subject.java

--- Subject.java
+++ Subject.java
@@ -21,3 +21,32 @@ public class Subject extends observable {
  
 
 }
+
+
+//Ha ach drt ana hhh
+
+public class Subject extends Observable {
+	
+    private int state;
+
+    public Subject(int val) {
+		this.setState(val);
+	}
+
+    public int getState() {
+    	return state;
+	}
+
+    public void setState(int state) {
+        this.state = state;
+        setChanged();
+        notifyObservers(state);
+    }
+	try {
+			while (!Thread.currentThread().isInterrupted()) {
+				this.setState((this.getState() + 1) % 60);
+				Thread.sleep(2000);
+			}
+		} catch (InterruptedException e) {
+			}
+}
